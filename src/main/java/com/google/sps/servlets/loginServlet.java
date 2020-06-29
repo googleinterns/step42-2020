85f784179005d9d4cd9d7d2de0a5b86343a7d5dd
Hannah Salameh <hannahsalameh@google.com>
2020-06-29 19:46:43 +0000

    basic authentication built

--- src/main/java/com/google/sps/servlets/loginServlet.java
+++ src/main/java/com/google/sps/servlets/loginServlet.java
@@ -32,43 +32,15 @@
 
 //   @Override
 //   public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException{
-//     GoogleIdTokenVerifier verifier = new GoogleIdTokenVerifier.Builder(transport, jsonFactory)
-//         // Specify the CLIENT_ID of the app that accesses the backend:
-//         .setAudience(Collections.singletonList(CLIENT_ID))
-//         // Or, if multiple clients access the backend:
-//         //.setAudience(Arrays.asList(CLIENT_ID_1, CLIENT_ID_2, CLIENT_ID_3))
-//         .build();
+//       String jsonString = request.getParameter("jsondata");
+//       JSONObject jsonObj = new JSONObject(jsonString);
 
-//     // (Receive idTokenString by HTTPS POST)
 
-//     GoogleIdToken idToken = verifier.verify(idTokenString);
-//     if (idToken != null) {
-//     Payload payload = idToken.getPayload();
-
-//     // Print user identifier
-//     String userId = payload.getSubject();
-//     System.out.println("User ID: " + userId);
-
-//     // Get profile information from payload
-//     String email = payload.getEmail();
-//     boolean emailVerified = Boolean.valueOf(payload.getEmailVerified());
-//     String name = (String) payload.get("name");
-//     String pictureUrl = (String) payload.get("picture");
-//     String locale = (String) payload.get("locale");
-//     String familyName = (String) payload.get("family_name");
-//     String givenName = (String) payload.get("given_name");
-
-//     // Use or store profile information
-//     // ...
-
-//     } else {
-//     System.out.println("Invalid ID token.");
-//     }
 // }
 
-//   @Override
-//   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-//   }
+// //   @Override
+// //   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+// //   }
 
 // }
 
--- src/main/webapp/loginScript.js
+++ src/main/webapp/loginScript.js
@@ -1,10 +1,37 @@
 function onSignIn(googleUser) {
   var profile = googleUser.getBasicProfile();
-  console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
-  console.log('Name: ' + profile.getName());
-  console.log('Image URL: ' + profile.getImageUrl());
-  console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
-  var id_token = googleUser.getAuthResponse().id_token;
-  
-  window.location.href = index.html;
+  localStorage.setItem("username",profile.getName()); //sends player's names into local storage
+  window.location = "index.html";
+}
+
+//adds username to each page
+document.addEventListener('DOMContentLoaded', (event) => {
+    var usernameElem = document.getElementById("username");
+    if(usernameElem){
+        usernameElem.innerHTML = localStorage.getItem("username");
+    }
+});
+
+function signOut() {
+  gapi.load('auth2', function() {
+       gapi.auth2.init({
+         client_id: '610251294652-9ojdhjhh8kpcdvdbmrvp9nkevgr2n2d8.apps.googleusercontent.com'
+    }).then(function(auth2){
+        auth2.signOut();
+        localStorage.removeItem("username");
+        window.location = "login.html";
+    });
+  });
+}
+
+function checkSignIn(){
+  gapi.load('auth2', function() {
+       gapi.auth2.init({
+         client_id: '610251294652-9ojdhjhh8kpcdvdbmrvp9nkevgr2n2d8.apps.googleusercontent.com'
+    }).then(function(auth2){
+        if(!auth2.isSignedIn.get()){
+            window.location.replace('login.html');
+        }
+    });
+});
 }
