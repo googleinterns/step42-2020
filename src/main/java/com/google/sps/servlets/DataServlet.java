449e328ca4ed26996f30685cca9855a17ec9b0ab
Emma Wenzel <emmawenzel512@gmail.com>
2020-07-01 19:44:01 +0000

    change DataServlet desciption to be more specific

--- src/main/java/com/google/sps/servlets/DataServlet.java
+++ src/main/java/com/google/sps/servlets/DataServlet.java
@@ -29,7 +29,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/** Stores and retrieves posts from Datastore */
+/** Stores blob keys for plant images in Datastore */
 @WebServlet("/data")
 public class DataServlet extends HttpServlet {
 
