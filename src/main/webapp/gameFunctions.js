5dbe29c8d7a99f78cbbaa1041a5848213de90809
liyahcoleman <colemanliyah@gmail.com>
2020-07-13 20:44:53 +0000

    created a class with helper functions to check if a user has taken a quiz. Also created the tests for these functions

--- src/main/webapp/gameFunctions.js
+++ src/main/webapp/gameFunctions.js
@@ -1,19 +1,11 @@
 //This is the js file for gameBoard.html
 
-// function getQuizStatus() {
-//     fetch("/getDailyQuiz").then(response => response.json()).then((tasks) => {
-//         console.log(tasks);
-//         if(tasks != "NoNewButton") {
-//             document.getElementById("QuizButton").style.display = "block";
-//         } 
-//     });
-// }
-
-function getQuizStatus() {
-    fetch("/user-quiz-status-servlet").then(response => response.json()).then((tasks) => {
-        console.log(tasks);
-        if(tasks==false){
-            document.getElementById("QuizButton").style.display = "block";
-        }
+function getUserQuizStatus() {
+    fetch("/game-quiz-status-servlet").then(response => response.json()).then((tasks) => {
+        fetch("/user-quiz-status-servlet").then(response => response.json()).then((tasks) => {
+            if(tasks==false){
+                document.getElementById("QuizButton").style.display = "block";
+            }
+        });
     });
 }
