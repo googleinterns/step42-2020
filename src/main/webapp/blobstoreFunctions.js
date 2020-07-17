cdb627bf2368e9b46043ab1e992c86166a937c5f
Emma Wenzel <emmawenzel512@gmail.com>
2020-07-17 19:41:02 +0000

    Functions to upload images and print them to the page, as well as the page for uploads

---
+++ src/main/webapp/blobstoreFunctions.js
@@ -0,0 +1,46 @@
+/** Loads photo */
+function loadScripts() {
+  fetchBlobstoreUrl();
+  imageToPage();
+}
+ 
+ 
+/** Loads action URL and attaches it to the upload form (the user's browser uploads the file to blobstore with the generated url) */
+function fetchBlobstoreUrl() {
+  fetch('/blobstore-upload-url').then((response) => response.text()).then((imageUploadUrl) => {
+    document.getElementById('plant-pic-form').action = imageUploadUrl;
+  });
+}
+ 
+ 
+/**
+  * Creates and adds a card element for the photo 
+  * pic is the URL of image to be shown in the post
+*/
+function createListElement(pic) {
+  let card = document.createElement("div");
+  let body = document.createElement("div");
+  let photo = document.createElement("img");
+  photo.style.width = "100px";
+ 
+  card.className = "card m-1";
+  body.className = "card-body";
+  photo.src = pic;
+ 
+  body.append(photo);
+  card.append(body);
+  document.getElementById('uploads').append(card);
+}
+ 
+/**
+  * Populates the list element with JSON fetched from the /get-blob-key servlet.
+*/
+function imageToPage() {
+  fetch('/get-blob-key').then(response => response.json()).then((blobKey) => {
+    if(blobKey != "noKey"){
+      fetch('/get-image?blobKey=' + blobKey).then((pic) => {
+        createListElement(pic.url);
+      });
+    }
+  });
+}
