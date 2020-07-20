a13a74e526203924d2389b0c39054fa6a35c6184
liyahcoleman <colemanliyah@gmail.com>
2020-07-20 20:38:18 +0000

    edited the game board html and js file

---
+++ src/main/webapp/gameFunctions.js
@@ -0,0 +1,7 @@
+function getUserQuizStatus() {
+    fetch("/user-quiz-status-servlet").then(response => response.json()).then((user_has_taken_quiz) => {
+        if(!user_has_taken_quiz){
+            document.getElementById("QuizButton").style.display = "block";
+        }
+    });
+}
