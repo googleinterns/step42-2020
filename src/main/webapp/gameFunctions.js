f6213e4e5ca55032fdf959cae3fd6e2713cd0662
liyahcoleman <colemanliyah@gmail.com>
2020-07-20 17:07:29 +0000

    Game page html and js file

---
+++ src/main/webapp/gameFunctions.js
@@ -0,0 +1,7 @@
+function getUserQuizStatus() {
+    fetch("/user-quiz-status-servlet").then(response => response.json()).then((tasks) => {
+        if(tasks==false){
+            document.getElementById("QuizButton").style.display = "block";
+        }
+    });
+}
