a974c236c301ed1eb2b245d24caff9680eb3a9ac
liyahcoleman <colemanliyah@gmail.com>
2020-07-22 18:08:02 +0000

    added documentation and changed variable names

--- src/main/java/com/google/sps/servlets/UserQuizStatusServlet.java
+++ src/main/java/com/google/sps/servlets/UserQuizStatusServlet.java
@@ -18,6 +18,8 @@ import com.google.appengine.api.datastore.Key;
 import com.google.sps.QuizTimingPropertiesUtils;
 import com.google.sps.UserUtils;
  
+//This servlet checks to see if the user took the quiz that was available today
+//If they did take the quiz the true boolean value will be returned and used when other functions/servlets are called in the js files
 @WebServlet("/user-quiz-status-servlet")
 public class UserQuizStatusServlet extends HttpServlet {
  
@@ -37,12 +39,13 @@ public class UserQuizStatusServlet extends HttpServlet {
         }
  
         QuizTimingPropertiesUtils timing_utils = new QuizTimingPropertiesUtils();
-        Long current_quiz_time = timing_utils.getQuizTimestampProperty("Game", "currentGame", userEntity.getProperty("currentGame").toString(), datastore);
-        Long user_quiz_status = timing_utils.getQuizTimestampProperty("user", "userID", userEntity.getProperty("userID").toString(), datastore);
+        //These variables get the last time the user took a quiz and the latest time a quiz was created
+        Long time_quiz_was_made = timing_utils.getQuizTimestampProperty("Game", "currentGame", userEntity.getProperty("currentGame").toString(), datastore);
+        Long latest_time_user_took_quiz = timing_utils.getQuizTimestampProperty("user", "userID", userEntity.getProperty("userID").toString(), datastore);
  
         Gson gson = new Gson();
         response.setContentType("application/json;");
  
-        response.getWriter().println(gson.toJson(timing_utils.userTookQuiz(user_quiz_status, current_quiz_time)));
+        response.getWriter().println(gson.toJson(timing_utils.userTookQuiz(latest_time_user_took_quiz, current_quiz_time)));
     }
 }
