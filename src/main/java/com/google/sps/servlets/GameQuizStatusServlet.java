a446175fcc9a816fbdd965e86bcdf89d3f1d900c
liyahcoleman <colemanliyah@gmail.com>
2020-07-23 23:19:21 +0000

    something fancy, today was a nice day

--- src/main/java/com/google/sps/servlets/GameQuizStatusServlet.java
+++ src/main/java/com/google/sps/servlets/GameQuizStatusServlet.java
@@ -14,7 +14,10 @@ import javax.servlet.http.HttpServletResponse;
 import com.google.appengine.api.datastore.FetchOptions;
 import com.google.appengine.api.datastore.Key;
 import com.google.sps.QuizTimingPropertiesUtils;
-//import com.google.sps.UserUtils;
+import com.google.sps.utils.UserUtils;
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
 
 //This servlet gets the quiz question from the Game entity 
 //The quiz question is updated if the quiz timestamp is outdated
@@ -45,6 +48,8 @@ public class GameQuizStatusServlet extends HttpServlet {
         QuizTimingPropertiesUtils timing_properties = new QuizTimingPropertiesUtils();
         //UserUtils user_utils_class = new UserUtils();
         Long current_quiz_stamp = timing_properties.getQuizTimestampProperty("Game", "gameID", userEntity.getProperty("currentGame").toString(), datastore);
+
+        Entity current_game = getEntityFromDatastore("Game", "gameID", userEntity.getProperty("currentGame").toString(), datastore);        
 
         Gson gson = new Gson();
         response.setContentType("application/json;");
