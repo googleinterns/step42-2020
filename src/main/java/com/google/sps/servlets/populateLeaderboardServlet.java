31001528c4416f85d49248b94b5acfa4c04d67a2
Emma Wenzel <emmawenzel512@gmail.com>
2020-07-24 22:31:39 +0000

    leaderboard first draft

--- src/main/java/com/google/sps/servlets/populateLeaderboardServlet.java
+++ src/main/java/com/google/sps/servlets/populateLeaderboardServlet.java
@@ -1,61 +1,71 @@
-// // Copyright 2019 Google LLC
-// //
-// // Licensed under the Apache License, Version 2.0 (the "License");
-// // you may not use this file except in compliance with the License.
-// // You may obtain a copy of the License at
-// //
-// //     https://www.apache.org/licenses/LICENSE-2.0
-// //
-// // Unless required by applicable law or agreed to in writing, software
-// // distributed under the License is distributed on an "AS IS" BASIS,
-// // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// // See the License for the specific language governing permissions and
-// // limitations under the License.
+// Copyright 2019 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
-// package com.google.sps.servlets;
+package com.google.sps.servlets;
 
-// import com.google.appengine.api.datastore.DatastoreService;
-// import com.google.appengine.api.datastore.DatastoreServiceFactory;
-// import com.google.appengine.api.datastore.Entity;
-// import com.google.appengine.api.datastore.PreparedQuery;
-// import com.google.appengine.api.datastore.Query;
-// import com.google.appengine.api.datastore.Query.SortDirection;
-// import java.io.IOException;
-// import javax.servlet.annotation.WebServlet;
-// import javax.servlet.http.HttpServlet;
-// import javax.servlet.http.HttpServletRequest;
-// import javax.servlet.http.HttpServletResponse;
-// import com.google.gson.Gson;
-// import java.util.ArrayList;
-// import com.google.sps.utils.User;
+import com.google.appengine.api.datastore.DatastoreService;
+import com.google.appengine.api.datastore.DatastoreServiceFactory;
+import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.datastore.PreparedQuery;
+import com.google.appengine.api.datastore.Query;
+import com.google.appengine.api.datastore.Query.SortDirection;
+import java.io.IOException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import com.google.gson.Gson;
+import java.util.ArrayList;
+import com.google.sps.utils.User;
+import com.google.sps.utils.UserUtils;
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
 
-// /** Servlet that adds and removes comments from the page */
-// @WebServlet("/populate-leaderboard")
-// public class populateLeaderboardServlet extends HttpServlet {
+/** Servlet that adds and removes comments from the page */
+@WebServlet("/populate-leaderboard")
+public class populateLeaderboardServlet extends HttpServlet {
+
+  DatastoreService datastore;
+ 
+  public populateLeaderboardServlet(){
+    datastore = DatastoreServiceFactory.getDatastoreService();
+  }
   
-//   /**
-//   * 
-//   */
-//   @Override
-//   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+  /**
+  * 
+  */
+  @Override
+  public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
  
-//     // get user entity
-//     Cookie cookies[] = request.getCookies();
-//     if(cookies == null){
-//         response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
-//         return;
-//     }
-//     Entity userEntity = UserUtils.getUserFromCookie(cookies, datastore);
-//     if(userEntity == null){
-//         response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
-//         return;
-//     }
+    // get user entity
+    Cookie cookies[] = request.getCookies();
+    if(cookies == null){
+        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        return;
+    }
+    Entity userEntity = UserUtils.getUserFromCookie(cookies, datastore);
+    if(userEntity == null){
+        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        return;
+    }
 
-//     ArrayList<User> users = UserUtils.userList((String) userEntity.getProperty("gameId"), datastore);
+    ArrayList<User> users = UserUtils.userList((String) userEntity.getProperty("gameId"), datastore);
  
-//     // translate to JSON for loadComments function
-//     Gson gson = new Gson();
-//     response.setContentType("application/json;");
-//     response.getWriter().println(gson.toJson(users));
-//   }
-// }
+    // translate to JSON for loadComments function
+    Gson gson = new Gson();
+    response.setContentType("application/json;");
+    response.getWriter().println(gson.toJson(users));
+  }
+}
--- src/main/webapp/gameFunctions.js
+++ src/main/webapp/gameFunctions.js
@@ -5,3 +5,39 @@ function getUserQuizStatus() {
         }
     });
 }
+
+/** 
+* Fetches number of comments to load, then fetches comments from the server 
+* and adds the requested number to the page 
+*/
+function loadUsers() {
+    fetch('/populate-leaderboard').then(response => response.json()).then((users) => {
+        const userListElement = document.getElementById('leader-board');
+        for(i = 0; i < users.size(); i++){
+            userListElement.appendChild(createUserElement(users[i]));
+        }
+    });
+}
+
+/** Creates a list item that contains the user */
+function createUserElement(user){
+  
+  // create list elements
+  const userElement = document.createElement('li');
+  const userIDElement = document.createElement('div');
+  const userNameElement = document.createElement('div');
+  const scoreElement = document.createElement('div');
+  const spacing = document.createElement('br');
+ 
+  // populate list elements with name and comment
+  userIDElement.innerText = user.userID;
+  userNameElement.innerText = ("-" + user.userName);
+  scoreElement.innerText = ("Score: " + user.score);
+  
+  // add to list item
+  userElement.appendChild(userNameElement);
+  userElement.appendChild(userIDElement);
+  userElement.appendChild(scoreElement);
+  userElement.appendChild(spacing);
+  return userElement;
+}
