1c46ff729709752f13a978ddd7e2aaeb90882f18
liyahcoleman <colemanliyah@gmail.com>
2020-07-24 23:17:01 +0000

    almost there

--- src/main/java/com/google/sps/servlets/getUserImagesForQuizPage.java
+++ src/main/java/com/google/sps/servlets/getUserImagesForQuizPage.java
@@ -13,7 +13,11 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import com.google.appengine.api.datastore.FetchOptions;
 import com.google.appengine.api.datastore.Key;
-import com.google.sps.QuizTimingPropertiesUtils;
+import com.google.sps.utils.QuizTimingPropertiesUtils;
+import com.google.sps.utils.UserUtils;
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
 import java.util.List;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -24,36 +28,23 @@ public class getUserImagesForQuizPage extends HttpServlet {
     DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        // Cookie cookies[] = request.getCookies();
-        // Entity userEntity = UserUtils.getUserFromCookie(cookies, datastore);
+        Cookie cookies[] = request.getCookies();
+        if(cookies == null){
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            return;
+        }
+        Entity userEntity = UserUtils.getUserFromCookie(cookies, datastore);
+        if(userEntity == null){
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            return;
+        } 
 
-        //Get cookies of user and then the current game from the user 
-
-        Query query = new Query("user");
-        PreparedQuery pq = datastore.prepare(query);
- 
-        Entity user_entity = pq.asList(FetchOptions.Builder.withLimit(1)).get(0);
-
-        Query queryTwo = new Query("Game");
-        PreparedQuery pqTwo = datastore.prepare(queryTwo);
-
-        HashMap<String, HashMap> user_photo_info = new HashMap<String, HashMap>();
         HashMap<String,String> user_ids_and_pictures = new HashMap<String, String>();
 
-        for(Entity game : pqTwo.asIterable()){
-            if(game.getProperty("gameID").equals(user_entity.getProperty("currentGame"))){
-                for(String playerID : (ArrayList<String>) game.getProperty("userIDs")) {
-                    for(Entity player : pq.asIterable()) {
-                        if(player.getProperty("userID").equals(playerID) && !user_entity.getProperty("userID").equals(playerID)) {
-                            System.out.println("twice");
-                            user_ids_and_pictures.put((player.getProperty("userID")).toString(), (player.getProperty("blobkey")).toString());
-                            // users_playing_the_game.add((player.getProperty("userID")).toString());
-                            // users_playing_blob_keys.add((player.getProperty("blobkey")).toString());
-                        }
-                    }
-                    // user_ids_and_pictures.put(users_playing_the_game, users_playing_blob_keys);
-                    // user_photo_info.put("user", user_ids_and_pictures);
-                }
+        current_game = UserUtils.getEntityFromDatastore("Game", "gameId", (userEntity.getProperty("gameId")).toString(), datastore);
+        for(String playerID : (ArrayList<String>) current_game.getProperty("userIDs")) {
+            if(player.getProperty("userID").equals(playerID) && !user_entity.getProperty("userID").equals(playerID)) {
+                user_ids_and_pictures.put((player.getProperty("userIds")).toString(), (player.getProperty("blobkey")).toString());
             }
         }
 
