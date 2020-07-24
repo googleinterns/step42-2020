85a8b118ce386a6d47d3fe6d61c0e2b05a1d9084
liyahcoleman <colemanliyah@gmail.com>
2020-07-24 23:00:43 +0000

    Our stuff works deployed

--- src/main/java/com/google/sps/servlets/GameQuizStatusServlet.java
+++ src/main/java/com/google/sps/servlets/GameQuizStatusServlet.java
@@ -64,6 +64,7 @@ public class GameQuizStatusServlet extends HttpServlet {
         if(QuizTimingPropertiesUtils.isTimestampOutdated(current_quiz_stamp)) {
             //response.getWriter().println(gson.toJson(timing_properties.getNewQuestion(game_entity, datastore)));
             response.getWriter().println(gson.toJson(QuizTimingPropertiesUtils.getNewQuestion(current_game, datastore)));
+            return;
 
         }
         
