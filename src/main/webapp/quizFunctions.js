0c511974b4a62dd9ef4fb495ba43a66359aaff39
liyahcoleman <colemanliyah@gmail.com>
2020-07-24 18:42:11 +0000

    movin on up

--- src/main/webapp/quizFunctions.js
+++ src/main/webapp/quizFunctions.js
@@ -15,28 +15,38 @@ if(document.getElementById("sound").onclick = function() {
 });
 
 document.getElementById("start_quiz").onclick = function() {
-    fetch("/user-quiz-status-servlet").then(response => response.json()).then((tasks) => {
-        if(tasks == true){
-            console.log("You already took this quiz!");
-        } else {
-            fetch("/get-user-images").then(response => response.json()).then((players_ids_and_photos) => {
-                Object.keys(players_ids_and_photos).forEach(function(key) {
-                    fetch('/get-image?blobKey=' + players_ids_and_photos[key]).then((pic) => {
-                    let button_for_picture = document.createElement("BUTTON");
-                    button_for_picture.name="user_picture";
-                    button_for_picture.value=key;
-                    let picture = document.createElement("IMG");
-                    picture.src = pic;
-                    document.getElementById("quiz_photos").appendChild(button_for_picture).appendChild(picture);
-                    });
-                });
-            });
+document.getElementById("start_quiz").onclick = function() {
+    // fetch("/user-quiz-status-servlet").then(response => response.json()).then((tasks) => {
+    //     if(tasks == true){
+    //         console.log("You already took this quiz!");
+    //     } else {
+    //         fetch("/get-user-images").then(response => response.json()).then((players_ids_and_photos) => {
+    //             Object.keys(players_ids_and_photos).forEach(function(key) {
+    //                 fetch('/get-image?blobKey=' + players_ids_and_photos[key]).then((pic) => {
+    //                 let button_for_picture = document.createElement("BUTTON");
+    //                 button_for_picture.name="user_picture";
+    //                 button_for_picture.value=key;
+    //                 let picture = document.createElement("IMG");
+    //                 picture.src = pic;
+    //                 document.getElementById("quiz_photos").appendChild(button_for_picture).appendChild(picture);
+    //                 });
+    //             });
+    //         });
             fetch("/game-quiz-status-servlet").then(response => response.json()).then((quiz_question) => {
                 document.getElementById("questions").innerText = quiz_question;
                 document.getElementById("quiz_time").style.display = "block";
                 document.getElementById("start_quiz").style.display = "none";
+                let button_for_picture = document.createElement("BUTTON");
+                button_for_picture.name="user_picture";
+                button_for_picture.value="hij";
+                let picture = document.createElement("IMG");
+                picture.src = "/image/plantFav.png";
+                document.getElementById("quiz_photos").appendChild(button_for_picture).appendChild(picture);
             });
-        }
-    });
+//         }
+//     });
+ }
+
+
  }
 
